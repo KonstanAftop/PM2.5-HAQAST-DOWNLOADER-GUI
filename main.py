ddf8ee72e5bafb55ecf99f3657878c6a2e34d98e
Konstan Aftop <konstanaftopds@gmail.com>
2025-02-13 13:02:39 +0700

    pembuatan window awal dan entry

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import tkinter as tk
+
+window=tk.Tk()
+window.title("PM2.5 HAQAST GUI")
+window.minsize(800, 600)  # Minimum width: 300px, height: 200px
+window.maxsize(4000, 3000)  # Maximum width: 800px, height: 600px
+
+entry1=tk.Entry(window, width=10)
+entry1.pack(pady=10)
+
+
+
+
+
+
+
+
+
+window.mainloop()
